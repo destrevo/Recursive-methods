4866346d7065e8ab3d5164f3d5f6e8eea58ccc64
destrevo <maczko.andor@gmail.com>
2021-08-24 19:54:33 +0200

    getNames recursive

--- app.js
+++ app.js
@@ -26,3 +26,45 @@ let getFilenames = (fs) => {
 
 console.log(filesystem)
 console.log(getFilenames(filesystem))
+
+const myself = {
+    name: 'Max',
+    friends: [
+      {
+        name: 'Manuel',
+        friends: [
+          {
+            name: 'Chris',
+            friends: [
+              {
+                name: 'Hari'
+              },
+              {
+                name: 'Amilia'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Julia'
+      }
+    ]
+  };
+  
+  function getFriendNames(person) {
+    const collectedNames = [];
+  
+    if (!person.friends) {
+      return [];
+    }
+    
+    for (const friend of person.friends) {
+      collectedNames.push(friend.name);
+      collectedNames.push(...getFriendNames(friend));
+    }
+    
+    return collectedNames;
+  }
+  
+  console.log(getFriendNames(myself));
